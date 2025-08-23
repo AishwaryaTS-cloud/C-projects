de27685e3e170645e3d7e9512815cd21d79ac71f
Aishwarya T S <aishwaryatsgowda02@gmail.com>
2025-08-23 22:07:09 +0530

    adding the code of simple calculator

---
+++ Beginner/Utilities/Simple_Calculator/code.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <math.h> // for sqrt() and pow()
+
+int main()
+{
+    char op;
+    double a, b, res;
+
+    printf("Enter an operator (+, -, *, /, %%, ^, s):\n");
+    printf("  + for Addition\n");
+    printf("  - for Subtraction\n");
+    printf("  * for Multiplication\n");
+    printf("  / for Division\n");
+    printf("  %% for Modulus (integers only)\n");
+    printf("  ^ for Power (a^b)\n");
+    printf("  s for Square Root (first operand only)\n");
+    scanf(" %c", &op);
+
+    if (op == 's')
+    {
+        // Only one input needed
+        printf("Enter a number: \n");
+        scanf("%lf", &a);
+        if (a >= 0)
+            printf("Result = %.2lf\n", sqrt(a));
+        else
+            printf("Error! Square root of negative number not allowed\n");
+    }
+    else
+    {
+        // Two operands needed
+        printf("Enter two operands: ");
+        scanf("%lf %lf", &a, &b);
+
+        switch (op)
+        {
+        case '+':
+            res = a + b;
+            break;
+        case '-':
+            res = a - b;
+            break;
+        case '*':
+            res = a * b;
+            break;
+        case '/':
+            if (b != 0)
+                res = a / b;
+            else
+            {
+                printf("Error! Division by zero\n");
+                return 1;
+            }
+            break;
+        case '%':
+            if ((int)b != 0)
+                res = (int)a % (int)b;
+            else
+            {
+                printf("Error! Modulus by zero\n");
+                return 1;
+            }
+            break;
+        case '^':
+            res = pow(a, b);
+            break;
+        default:
+            printf("Error! Invalid operator\n");
+            return 1;
+        }
+        printf("Result = %.2lf\n", res);
+    }
+
+    return 0;
+}
